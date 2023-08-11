5640f07abaa34abc19a170ca4ca786eac9aaa93e
MikeRootCricut <44172744+MikeRootCricut@users.noreply.github.com>
2023-08-11 10:37:51 -0500

    Update Counter.tsx

--- app/components/Counter.tsx
+++ app/components/Counter.tsx
@@ -3,7 +3,7 @@ import "./Counter.css";
 
 function Counter() {
   const [value, setValue] = useState(0);
-  const decClass = value < -10 ? "Counter--btnhidden" : "Counter--btnvisible";
+  const decClass = value <= -10 ? "Counter--btnhidden" : "Counter--btnvisible";
   const incClass = value >= 10 ? "Counter--btnhidden" : "Counter--btnvisible";
   const message =
     value >= 8
@@ -15,10 +15,11 @@ function Counter() {
   return (
     <div className="Counter">
       <h2>Counter</h2>
-      <p>{value}</p>
+      <p data-testid="counter_value">{value}</p>
       <div className="Counter--btns">
         <button
           type="button"
+          disabled={value <= -10}
           className={"IncDec " + decClass}
           onClick={() => setValue(value - 1)}
         >
@@ -26,13 +27,14 @@ function Counter() {
         </button>
         <button
           type="button"
+          disabled={value >= 10}
           className={"IncDec " + incClass}
           onClick={() => setValue(value + 1)}
         >
           Increment
         </button>
       </div>
-      <p>{message}</p>
+      <p data-testid="message">{message}</p>
     </div>
   );
 }
